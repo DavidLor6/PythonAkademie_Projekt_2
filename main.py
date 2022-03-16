f2bc72ba4befedbac7230c99fd11df697c70b442
DavidLor6 <dav.lor@seznam.cz>
2022-03-16 22:12:29 +0100

    uvod

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Python Akademie - 2.projekt --- Bull & Cows
+
+# zakladni promenne
+oddelovac = "-" * 47
+
+
+
+# Zacatek programu
+print(
+    "Hi there!" +
+    "\n" + oddelovac +
+    "\n" + "I've generated a random 4 digit number for you." +
+    "\n" + "Let's play a bulls and cows game."
+    "\n" + oddelovac
+)
