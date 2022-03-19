beccfa0bbd8f7c10054d4de925cd7e2ab5fcf0c4
DavidLor6 <dav.lor@seznam.cz>
2022-03-19 21:43:00 +0100

    uvod

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 # Python Akademie - 2.projekt --- Bull & Cows
 
-# zakladni promenne
+# import knihoven
+import random
+
+# definovani promennych
 oddelovac = "-" * 47
+cislo = []
+pocet_pokusu = []
 
+# bulls = 0    # uhodnute cislo i umisteni
+# cows = 0     # uhodnute pouze cislo, ne umisteni
 
+# vytvoreni hadaneho cisla
+def generuj_cislo():
+    for i in range(4):
+        x = random.randrange(0, 9)
+        cislo.append(x)
+    if len(cislo) > len(set(cislo)):
+        cislo.clear()
+        generuj_cislo()
+
+print(cislo)
 
 # Zacatek programu
 print(
@@ -13,3 +30,4 @@ print(
     "\n" + "Let's play a bulls and cows game."
     "\n" + oddelovac
 )
+
