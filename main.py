a31fc25839a288938c50036a21a6a73bd4c46f2d
DavidLor6 <dav.lor@seznam.cz>
2022-03-20 21:21:42 +0100

    pokracovani 03

--- main.py
+++ main.py
@@ -21,13 +21,30 @@ def generuj_cislo():
         generuj_cislo()
 
 def hrat_hru():
-    # ??? global pocet_pokusu
-    pocet_pokusu =+ 1
+    global pocet_pokusu
+    pocet_pokusu += 1
     cows = 0
     bulls = 0
     pokus = input(">>> ")
     if len(pokus) != 4 or len(pokus) > len(set(pokus)) or int(pokus[0]) == 0:
         print("Wrong input! Your number has to have 4 digit, each digit has to be different and cannot start with 0")
+        hrat_hru()
+    for i in range(4):
+        if int(pokus[i]) == int(cislo[i]):
+            bulls += 1
+    for j in range(4):
+        if int(pokus[j]) in int(cislo):
+            cows +=1
+    cows = cows - bulls
+    if bulls <= 1 and cows <= 1:
+        print(f"{bulls} bull, {cows} cow")
+    elif bulls >= 1 and cows <= 1:
+        print(f"{bulls} bulls, {cows} cow")
+    elif bulls <= 1 and cows >= 1:
+        print(f"{bulls} bull, {cows} cows")
+    else:
+        print(f"{bulls} bulls, {cows} cows")
+
 
 
 
